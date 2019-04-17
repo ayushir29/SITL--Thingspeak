604fd26387a495dc2858f8e2abd7e8380a916bf4
ayushir29 <46811903+ayushir29@users.noreply.github.com>
2019-04-17 10:34:23 +0530

    Add files via upload

---
+++ iot.m
@@ -0,0 +1,6 @@
+clc;clear;
+for i=0:2:40
+    thingSpeakWrite(749365,[i,randi(100)],'writekey','FE2GSW6MV5ZEZQTC')
+    pause(40);
+end
+% thingSpeakRead(749365,'Fields',1,'OutputFormat','Timetable')
